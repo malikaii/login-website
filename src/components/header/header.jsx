3592667b9cbdcaca34650caeb303c5b063bb95eb
Malick Ouedraogo <C00400598@louisiana.edu>
2022-07-28 22:32:50 -0500

    Create the header component and added styling

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,16 @@
+import './header.css'
+
 const Header = () => {
-    return 
+    return (
+        <>
+            <nav className='navbar'>
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href='/'>Login</a></li>
+                </ul>
+            </nav>
+        </>
+    )
 }
 
 export default Header;
